10ba612629a578c695910d70c96d18b13eb350f5
aidelojep <aidelojepeter123@gmail.com>
2021-04-09 17:22:23 +0100

    Python practice

---
+++ DetailsCollection/CustomerDetails.py
@@ -0,0 +1,4 @@
+customer_first_name_str=input("Please enter your first Name")
+Customer_last_name_str=input("Please enter your last Name")
+Customer_date_of_birth=input("Please enter your dob")
+print (f"Your First name is {customer_first_name_str}, last Name is {Customer_last_name_str} and your age is {Customer_date_of_birth}")
